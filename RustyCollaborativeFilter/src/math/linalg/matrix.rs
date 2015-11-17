27c3fe33446ca5f22d05251b356fbaf2baff8ee4
James Lucas <james.lucas@loopup.com>
2015-11-16 16:29:59 -0800

    Adding more to stub

--- RustyCollaborativeFilter/src/math/linalg/matrix.rs
+++ RustyCollaborativeFilter/src/math/linalg/matrix.rs
@@ -1,4 +1,6 @@
 use std::ops::Mul;
+use std::ops::Add;
+use std::ops::Div;
 
 struct Matrix {
 	dim1: i32,
@@ -16,7 +18,23 @@ impl Mul<f32> for Matrix {
 impl Mul<Matrix> for Matrix {
 	type Output = Matrix;
 
+	fn mul(self, f: Matrix) -> Matrix {
+		// Will use Strassen algorithm if large, traditional otherwise
+	}
+}
+
+impl Add<Matrix> for Matrix {
+	type Output = Matrix;
+
+	fn mul(self, f: Matrix) -> Matrix {
+
+	}
+}
+
+impl Div<f32> for Matrix {
+	type Output = Matrix;
+
 	fn mul(self, f: f32) -> Matrix {
-		
+
 	}
 }
