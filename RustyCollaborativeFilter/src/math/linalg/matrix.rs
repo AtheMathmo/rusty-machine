18e110e551ff8bb1ba02de3750b7d668d82dc336
James Lucas <LucasJ94@hotmail.co.uk>
2015-11-16 22:03:51 -0800

    Implemented constructors. Minor impl updates.

--- RustyCollaborativeFilter/src/math/linalg/matrix.rs
+++ RustyCollaborativeFilter/src/math/linalg/matrix.rs
@@ -4,13 +4,52 @@ use std::ops::Div;
 
 struct Matrix {
 	cols: i32,
-	rows: i32
+	rows: i32,
+	data: Vec<f32>
+}
+
+impl Matrix {
+    fn new(cols: i32, rows: i32, data: Vec<f32>) -> Matrix {
+        Matrix {
+            cols: cols,
+            rows: rows,
+            data: data
+        }
+    }
+
+    fn zeros(cols: i32, rows: i32) -> Matrix {
+    	Matrix {
+            cols: cols,
+            rows: rows,
+            data: vec![0.0; (cols*rows) as usize]
+        }
+    }
+
+    fn identity(size: i32) -> Matrix {
+    	let mut data = vec![0.0; (size*size) as usize];
+
+    	for i in 0..size
+    	{
+    		data[(i*(size+1)) as usize] = 1.0;
+    	}
+
+    	Matrix {
+            cols: size,
+            rows: size,
+            data: data
+        }
+    }
 }
 
 impl Mul<f32> for Matrix {
 	type Output = Matrix;
 
 	fn mul(self, m: f32) -> Matrix {
+
+		for entry in &self.data {
+			entry * m;
+		}
+
 		return self;
 	}
 }
@@ -20,6 +59,18 @@ impl Mul<Matrix> for Matrix {
 
 	fn mul(self, m: Matrix) -> Matrix {
 		// Will use Strassen algorithm if large, traditional otherwise
+		return self;
+	}
+}
+
+impl Add<f32> for Matrix {
+	type Output = Matrix;
+
+	fn add(self, m: f32) -> Matrix {
+		for entry in &self.data {
+			entry + m;
+		}
+
 		return self;
 	}
 }
