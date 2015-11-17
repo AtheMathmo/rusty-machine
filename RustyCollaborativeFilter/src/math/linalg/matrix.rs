e40f45e614959686b0a1aa657b2fd2293dc49cdd
James Lucas <james.lucas@loopup.com>
2015-11-17 11:57:36 -0800

    Fixed basic matrix scalar multiplication.

--- RustyCollaborativeFilter/src/math/linalg/matrix.rs
+++ RustyCollaborativeFilter/src/math/linalg/matrix.rs
@@ -1,15 +1,13 @@
-use std::ops::Mul;
-use std::ops::Add;
-use std::ops::Div;
+use std::ops::{Mul, Add, Div, Sub, Index};
 
 struct Matrix {
-	cols: i32,
-	rows: i32,
+	cols: usize,
+	rows: usize,
 	data: Vec<f32>
 }
 
 impl Matrix {
-    fn new(cols: i32, rows: i32, data: Vec<f32>) -> Matrix {
+    fn new(cols: usize, rows: usize, data: Vec<f32>) -> Matrix {
         Matrix {
             cols: cols,
             rows: rows,
@@ -17,16 +15,16 @@ impl Matrix {
         }
     }
 
-    fn zeros(cols: i32, rows: i32) -> Matrix {
+    fn zeros(cols: usize, rows: usize) -> Matrix {
     	Matrix {
             cols: cols,
             rows: rows,
-            data: vec![0.0; (cols*rows) as usize]
+            data: vec![0.0; cols*rows]
         }
     }
 
-    fn identity(size: i32) -> Matrix {
-    	let mut data = vec![0.0; (size*size) as usize];
+    fn identity(size: usize) -> Matrix {
+    	let mut data = Vec::with_capacity(size * size);
 
     	for i in 0..size
     	{
@@ -39,19 +37,36 @@ impl Matrix {
             data: data
         }
     }
+
+    fn from_diag(diag: &Vec<f32>) -> Matrix {
+    	let size = diag.len();
+    	let mut data = Vec::with_capacity(size * size);
+
+    	for i in 0..size
+    	{
+    		data[(i*(size+1)) as usize] = diag[i];
+    	}
+
+    	Matrix {
+            cols: size,
+            rows: size,
+            data: data
+        }
+    }
 }
 
 impl Mul<f32> for Matrix {
-	type Output = Matrix;
+    type Output = Matrix;
 
-	fn mul(self, m: f32) -> Matrix {
+    fn mul(self, m: f32) -> Matrix {
+        let new_data = self.data.into_iter().map(|v| v * m).collect();
 
-		for entry in &self.data {
-			entry * m;
-		}
-
-		return self;
-	}
+        Matrix {
+            cols: self.cols,
+            rows: self.rows,
+            data: new_data
+        }
+    }
 }
 
 impl Mul<Matrix> for Matrix {
