49eae3396e6987b9baf7f0800ec4cd9b93235056
James Lucas <LucasJ94@hotmail.co.uk>
2015-12-12 20:20:04 -0800

    Bug fix.

--- rusty-machine/src/learning/lin_reg.rs
+++ rusty-machine/src/learning/lin_reg.rs
@@ -3,13 +3,13 @@ use math::linalg::matrix::Matrix;
 use math::linalg::vector::Vector;
 
 pub struct LinRegressor {
-	b: Vector<f64>
+	pub b: Vector<f64>
 }
 
 impl Model<Matrix<f64>, Vector<f64>> for LinRegressor {
 
 	fn predict(&self, data: Matrix<f64>) -> Vector<f64> {
-		data * b
+		data * &self.b
 	}
 
 	fn train(&mut self, data: Matrix<f64>, values: Vector<f64>) {
