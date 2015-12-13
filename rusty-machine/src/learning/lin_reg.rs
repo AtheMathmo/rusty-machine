c141c3d13c0f313ce25b1683f809e132ccd4181a
James Lucas <LucasJ94@hotmail.co.uk>
2015-12-12 22:38:06 -0800

    Added documentation for linear regression/.

--- rusty-machine/src/learning/lin_reg.rs
+++ rusty-machine/src/learning/lin_reg.rs
@@ -1,3 +1,11 @@
+//! Linear Regression module
+//! 
+//! Contains implementation of linear regression models.
+//! Allows training and prediction of linear regression model
+//! using least squares optimization.
+//!
+//! Currently only OLS solution - gradient descent not yet implemented.
+
 use learning::Model;
 use linalg::matrix::Matrix;
 use linalg::vector::Vector;
@@ -7,6 +15,16 @@ pub struct LinRegressor {
 }
 
 impl LinRegressor {
+
+	/// Constructs untrained linear regression model.
+	///
+	/// # Examples
+	///
+	/// ```
+	/// use rusty_machine::learning::lin_reg::LinRegressor;
+	///
+	/// let mut lin_mod = LinRegressor::new();
+	/// ```
 	pub fn new() -> LinRegressor {
 		LinRegressor { b: None }
 	}
@@ -14,16 +32,32 @@ impl LinRegressor {
 
 impl Model<Matrix<f64>, Vector<f64>> for LinRegressor {
 
+	/// Train the linear regression model.
+	///
+	/// Takes training data and output values as input.
+	///
+	/// # Examples
+	///
+	/// ```
+	/// let mut lin_mod = LinRegressor::new();
+	/// let data = Matrix::new(3,2, vec![1.0, 2.0, 1.0, 3.0, 1.0, 4.0]);
+	/// let values = Vector::new(vec![5.0, 6.0, 7.0]);
+	///
+	/// lin_mod.train(data, values);
+	/// ```
+	fn train(&mut self, data: Matrix<f64>, values: Vector<f64>) {
+		let xt = data.transpose();
+
+		self.b = Some(((&xt * data).inverse() * &xt) * values);
+	}
+
+	/// Predict output value from input data.
+	///
+	/// Model must be trained before prediction can be made.
 	fn predict(&self, data: Matrix<f64>) -> Vector<f64> {
 		match self.b {
 			Some(ref v) => data * v,
 			None => panic!("Model has not been trained.")
 		}
-	}
-
-	fn train(&mut self, data: Matrix<f64>, values: Vector<f64>) {
-		let xt = data.transpose();
-
-		self.b = Some(((&xt * data).inverse() * &xt) * values);
 	}
 }
