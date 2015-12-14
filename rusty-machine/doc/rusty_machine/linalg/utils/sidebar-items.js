1deb2e74897d60ebe27d3fdd558d911ae13bc35c
James Lucas <LucasJ94@hotmail.co.uk>
2015-12-13 22:58:50 -0800

    Updating document pages.

--- rusty-machine/doc/rusty_machine/linalg/utils/sidebar-items.js
+++ rusty-machine/doc/rusty_machine/linalg/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["argmax","Find argmax of slice."],["dot","Compute dot product of two slices."],["find","Find index of value in slice."],["unrolled_sum","Compute sum of two slices."]]});
+initSidebarItems({"fn":[["argmax","Find argmax of slice."],["dot","Compute dot product of two slices."],["find","Find index of value in slice."],["vec_sub","Compute vector difference two slices."],["vec_sum","Compute vector sum of two slices."]]});
