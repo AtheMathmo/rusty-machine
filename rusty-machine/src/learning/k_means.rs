af018b321f0fda1b89f4ceb1980f86902a0ac9c0
James Lucas <LucasJ94@hotmail.co.uk>
2015-12-17 09:05:33 -0800

    Filling out code for k-means.

--- rusty-machine/src/learning/k_means.rs
+++ rusty-machine/src/learning/k_means.rs
@@ -14,51 +14,60 @@ impl UnSupModel<Matrix<f64>, Vector<usize>> for KMeansClassifier {
     }
 
     fn train(&mut self, data: Matrix<f64>) {
-    	self.init_centroids(data.cols);
+        self.init_centroids(data.cols);
 
-    	for i in 0..self.iters {
-    		let idx = self.find_closest_centroids(&data);
-    		self.compute_means(&data, idx);
-    	}
+        for i in 0..self.iters {
+            let idx = self.find_closest_centroids(&data);
+            self.compute_means(&data, idx);
+        }
     }
 }
 
 impl KMeansClassifier {
-	pub fn new(k: usize) -> KMeansClassifier {
-		KMeansClassifier{ iters: 100, k:k, centroids: None}
-	}
+    pub fn new(k: usize) -> KMeansClassifier {
+        KMeansClassifier {
+            iters: 100,
+            k: k,
+            centroids: None,
+        }
+    }
 
     fn init_centroids(&mut self, dim: usize) {
-    	// These should not all be equal!
-    	self.centroids = Some(Matrix::zeros(self.k, dim));
+        // These should not all be equal!
+        self.centroids = Some(Matrix::zeros(self.k, dim));
     }
 
     fn find_closest_centroids(&self, data: &Matrix<f64>) -> Vector<usize> {
-    	let mut idx = Vector::zeros(data.rows);
+        let mut idx = Vector::zeros(data.rows);
 
-    	for i in 0..data.rows {
-    		for j in 0..self.k {
-    			// idx[i] = argmin distance to centroid;
-    		}
-    	}
-    	idx
+        match self.centroids {
+            Some(ref c) => {
+                for i in 0..data.rows {
+                    let centroid_dist = c - data.select_rows(&vec![i; c.rows]);
+                    let dist = &centroid_dist.elemul(&centroid_dist).sum_cols();
+                    // Now take argmin and this is the centroid.
+
+                }
+            }
+            None => panic!("Centroids not defined."),
+        }
+
+        idx
     }
 
-    fn compute_means(&mut self, data: &Matrix<f64>, classes: Vector<usize>)
-    {
-    	for i in 0..self.k {
-    		let mut vec_k = Vec::new();
+    fn compute_means(&mut self, data: &Matrix<f64>, classes: Vector<usize>) {
+        for i in 0..self.k {
+            let mut vec_k = Vec::new();
 
-    		for j in classes.data.iter() {
-    			if *j == i {
-    				vec_k.push(j);
-    			}
-    		}
+            for j in classes.data.iter() {
+                if *j == i {
+                    vec_k.push(j);
+                }
+            }
 
-    		// index out these rows from data
-    		// make a new matrix
-    		// compute average of it's rows
-    	}
+            // index out these rows from data
+            // make a new matrix
+            // compute average of it's rows
+        }
     }
 }
-
