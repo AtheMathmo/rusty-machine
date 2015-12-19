d1040efdd73da144e122ecb428389c4a24f3926a
James Lucas <LucasJ94@hotmail.co.uk>
2015-12-18 21:59:34 -0800

    Adding benchmark test for detemrinant.

--- rusty-machine/benches/linalg/matrix.rs
+++ rusty-machine/benches/linalg/matrix.rs
@@ -10,7 +10,22 @@ fn empty(b: &mut Bencher) {
 fn mat_mul(b: &mut Bencher) {
 
 	let a = Matrix::new(10,10, vec![2.0;100]);
-	let c = Matrix::new(10,10, vec![3.0; 100]);
+	let c = Matrix::new(10,10, vec![3.0;100]);
 
     b.iter(|| &a * &c)
+}
+
+#[bench]
+fn mat_add(b: &mut Bencher) {
+	let a = Matrix::new(10,10, vec![2.0;100]);
+	let c = Matrix::new(10,10, vec![3.0;100]);
+
+    b.iter(|| &a + &c)
+}
+
+#[bench]
+fn mat_det(b: &mut Bencher) {
+	let a = Matrix::new(10,10, vec![2.0;100]);
+
+	b.iter(|| a.det());
 }
