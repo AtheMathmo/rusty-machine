db75d24ebed15e901488be82816404ac06312b9d
James Lucas <LucasJ94@hotmail.co.uk>
2015-12-29 18:40:53 -0800

    Added basic training test for neural nets.

---
+++ rusty-machine/tests/learning/nnet.rs
@@ -0,0 +1,17 @@
+use rm::linalg::matrix::Matrix;
+use rm::learning::SupModel;
+use rm::learning::nnet::NeuralNet;
+
+#[test]
+fn test_model() {
+
+	let data = Matrix::new(5,3, vec![1.,1.,1.,2.,2.,2.,3.,3.,3.,
+									4.,4.,4.,5.,5.,5.,]);
+	let outputs = Matrix::new(5,3, vec![1.,0.,0.,0.,1.,0.,0.,0.,1.,
+										0.,0.,1.,0.,0.,1.]);
+
+	let layers = &[3,5,3];
+	let mut model = NeuralNet::new(layers);
+
+	model.train(data, outputs);
+}
