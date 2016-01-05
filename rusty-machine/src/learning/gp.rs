f003bc99f56a606ab152ebd9ee9ceebbd5333f04
James Lucas <LucasJ94@hotmail.co.uk>
2016-01-05 12:14:04 -0800

    First pass at GP regression.

--- rusty-machine/src/learning/gp.rs
+++ rusty-machine/src/learning/gp.rs
@@ -3,7 +3,7 @@ use learning::SupModel;
 use linalg::matrix::Matrix;
 
 trait MeanFunc {
-    fn func(&self, x: f64) -> f64;
+    fn func(&self, x: Matrix<f64>) -> Matrix<f64>;
 }
 
 struct ConstMean {
@@ -17,8 +17,8 @@ impl Default for ConstMean {
 }
 
 impl MeanFunc for ConstMean {
-    fn func(&self, _: f64) -> f64 {
-        self.a
+    fn func(&self, x: Matrix<f64>) -> Matrix<f64> {
+        Matrix::ones(x.rows(), x.cols()) * self.a
     }
 }
 
@@ -65,22 +65,31 @@ impl<T: Kernel, U: MeanFunc> GaussianProcess<T, U> {
 }
 
 impl<T: Kernel, U: MeanFunc> SupModel<Matrix<f64>, Matrix<f64>> for GaussianProcess<T, U> {
+    /// Predict output from data.
+    fn predict(&self, data: &Matrix<f64>) -> Matrix<f64> {
+        let mean = self.mean.func(data.clone());
 
-	/// Predict output from data.
-        fn predict(&self, data: &Matrix<f64>) -> Matrix<f64> {
-        	Matrix::new(1,1,vec![1f64])
+        // Messy referencing for succint syntax
+        if let (&Some(ref t_data), &Some(ref t_mat), &Some(ref t_out)) = (&self.train_data, &self.train_mat, &self.train_output) {
+        	let test_mat = self.ker_mat(data, t_data) * t_mat *
+                       (t_out - self.mean.func(t_data.clone()));
+        	return mean + test_mat;
         }
 
-        /// Train the model using data and outputs.
-        fn train(&mut self, data: &Matrix<f64>, value: &Matrix<f64>) {
-        	let noise_mat = Matrix::identity(data.rows()) * self.noise;
+        panic!("The model has not been trained.");
+        
+    }
 
-        	let ker_mat = self.ker_mat(data, data);
+    /// Train the model using data and outputs.
+    fn train(&mut self, data: &Matrix<f64>, value: &Matrix<f64>) {
+        let noise_mat = Matrix::identity(data.rows()) * self.noise;
 
-        	let train_mat = (ker_mat + noise_mat).inverse();
+        let ker_mat = self.ker_mat(data, data);
 
-        	self.train_mat = Some(train_mat);
-        	self.train_data = Some(data.clone());
-        	self.train_output = Some(value.clone());
-        }
+        let train_mat = (ker_mat + noise_mat).inverse();
+
+        self.train_mat = Some(train_mat);
+        self.train_data = Some(data.clone());
+        self.train_output = Some(value.clone());
+    }
 }
