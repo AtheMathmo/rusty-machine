4b340852cf54d92c6837e6ec4e6aeacb637cb9b8
James Lucas <LucasJ94@hotmail.co.uk>
2016-01-05 19:26:20 -0800

    Changing ampl for numeric stability

--- rusty-machine/tests/learning/gp.rs
+++ rusty-machine/tests/learning/gp.rs
@@ -5,6 +5,7 @@ use rm::learning::gp::GaussianProcess;
 #[test]
 fn test_default_gp() {
 	let mut gp = GaussianProcess::default();
+	gp.noise = 10f64;
 
 	let train_data = Matrix::new(10,1,vec![0.,1.,2.,3.,4.,5.,6.,7.,8.,9.]);
 	let target = Matrix::new(10,1,vec![0.,1.,2.,3.,4.,4.,3.,2.,1.,0.]);
