0516bdda2374b2e1a6c947bff40d1c8975495d17
James Lucas <LucasJ94@hotmail.co.uk>
2016-01-14 09:03:08 -0800

    Updating GP to use cholesky.

--- rusty-machine/src/learning/gp.rs
+++ rusty-machine/src/learning/gp.rs
@@ -3,7 +3,7 @@
 //! Provides implementation of gaussian process regression.
 //!
 //! # Usage
-//! 
+//!
 //! ```
 //! use rusty_machine::learning::gp;
 //! use rusty_machine::learning::SupModel;
@@ -23,16 +23,17 @@
 //! ```
 //! Alternatively one could use gaussp.get_posterior() which would return both
 //! the predictive mean and covariance. However, this is likely to change in
-//! a future release. 
+//! a future release.
 
 use learning::toolkit::kernel::{Kernel, SquaredExp};
 use learning::SupModel;
 use linalg::matrix::Matrix;
+use linalg::vector::Vector;
 
 /// Trait for GP mean functions.
 pub trait MeanFunc {
     /// Compute the mean function applied elementwise to a matrix.
-    fn func(&self, x: Matrix<f64>) -> Matrix<f64>;
+    fn func(&self, x: Matrix<f64>) -> Vector<f64>;
 }
 
 /// Constant mean function
@@ -41,7 +42,6 @@ pub struct ConstMean {
 }
 
 impl Default for ConstMean {
-
     /// Constructs the zero function.
     ///
     /// # Examples
@@ -56,8 +56,8 @@ impl Default for ConstMean {
 }
 
 impl MeanFunc for ConstMean {
-    fn func(&self, x: Matrix<f64>) -> Matrix<f64> {
-        Matrix::zeros(x.rows(), x.cols()) + self.a
+    fn func(&self, x: Matrix<f64>) -> Vector<f64> {
+        Vector::zeros(x.rows()) + self.a
     }
 }
 
@@ -71,9 +71,9 @@ pub struct GaussianProcess<T: Kernel, U: MeanFunc> {
     mean: U,
     /// The observation noise of the GP.
     pub noise: f64,
-    train_data: Option<Matrix<f64>>,
-    train_output: Option<Matrix<f64>>,
+    alpha: Option<Vector<f64>>,
     train_mat: Option<Matrix<f64>>,
+    train_data: Option<Matrix<f64>>,
 }
 
 impl Default for GaussianProcess<SquaredExp, ConstMean> {
@@ -90,10 +90,10 @@ impl Default for GaussianProcess<SquaredExp, ConstMean> {
         GaussianProcess {
             ker: SquaredExp::default(),
             mean: ConstMean::default(),
-            noise: 1f64,
-            train_data: None,
-            train_output: None,
+            noise: 0f64,
             train_mat: None,
+            train_data: None,
+            alpha: None,
         }
     }
 }
@@ -116,15 +116,17 @@ impl<T: Kernel, U: MeanFunc> GaussianProcess<T, U> {
             ker: ker,
             mean: mean,
             noise: noise,
-            train_data: None,
-            train_output: None,
             train_mat: None,
+            train_data: None,
+            alpha: None,
         }
     }
 
     /// Construct a kernel matrix
     fn ker_mat(&self, m1: &Matrix<f64>, m2: &Matrix<f64>) -> Matrix<f64> {
         assert_eq!(m1.cols(), m2.cols());
+        let cols = m1.cols();
+
         let dim1 = m1.rows();
         let dim2 = m2.rows();
 
@@ -132,8 +134,8 @@ impl<T: Kernel, U: MeanFunc> GaussianProcess<T, U> {
 
         for i in 0..dim1 {
             for j in 0..dim2 {
-                ker_data.push(self.ker.kernel(&m1.data()[i * m1.cols()..(i + 1) * m1.cols()],
-                                              &m2.data()[j * m2.cols()..(j + 1) * m2.cols()]));
+                ker_data.push(self.ker.kernel(&m1.data()[i * cols..(i + 1) * cols],
+                                              &m2.data()[j * cols..(j + 1) * cols]));
             }
         }
 
@@ -141,34 +143,41 @@ impl<T: Kernel, U: MeanFunc> GaussianProcess<T, U> {
     }
 }
 
-impl<T: Kernel, U: MeanFunc> SupModel<Matrix<f64>, Matrix<f64>> for GaussianProcess<T, U> {
+impl<T: Kernel, U: MeanFunc> SupModel<Matrix<f64>, Vector<f64>> for GaussianProcess<T, U> {
     /// Predict output from data.
-    fn predict(&self, data: &Matrix<f64>) -> Matrix<f64> {
-        let mean = self.mean.func(data.clone());
+    fn predict(&self, data: &Matrix<f64>) -> Vector<f64> {
 
         // Messy referencing for succint syntax
-        if let (&Some(ref t_data), &Some(ref t_mat), &Some(ref t_out)) = (&self.train_data,
-                                                                          &self.train_mat,
-                                                                          &self.train_output) {
-            let test_mat = self.ker_mat(data, t_data) * t_mat *
-                           (t_out - self.mean.func(t_data.clone()));
-            return mean + test_mat;
+        if let (&Some(ref alpha), &Some(ref t_data)) = (&self.alpha, &self.train_data) {
+            let mean = self.mean.func(data.clone());
+
+            let post_mean = self.ker_mat(data, t_data) * alpha;
+
+            return mean + post_mean;
+
         }
 
         panic!("The model has not been trained.");
     }
 
     /// Train the model using data and outputs.
-    fn train(&mut self, data: &Matrix<f64>, value: &Matrix<f64>) {
+    fn train(&mut self, data: &Matrix<f64>, value: &Vector<f64>) {
         let noise_mat = Matrix::identity(data.rows()) * self.noise;
 
         let ker_mat = self.ker_mat(data, data);
 
-        let train_mat = (ker_mat + noise_mat).inverse();
+        let train_mat = (ker_mat + noise_mat).cholesky();
+
+        let x = GaussianProcess::<T, U>::solve_l_triangular(&train_mat,
+                                                            &(value -
+                                                              self.mean.func(data.clone())));
+        let alpha = GaussianProcess::<T, U>::solve_u_triangular(&train_mat.transpose(), &x);
+
+        println!("x {:?}", x.data());
 
         self.train_mat = Some(train_mat);
         self.train_data = Some(data.clone());
-        self.train_output = Some(value.clone());
+        self.alpha = Some(alpha);
     }
 }
 
@@ -178,21 +187,44 @@ impl<T: Kernel, U: MeanFunc> GaussianProcess<T, U> {
     /// Requires the model to be trained first. 
     /// _Note that this is a messy compromise as GPs do not
     /// fit the SupModel trait as is currently implemented._
-    pub fn get_posterior(&self, data: &Matrix<f64>) -> (Matrix<f64>, Matrix<f64>) {
-        let mean = self.mean.func(data.clone());
+    pub fn get_posterior(&self, data: &Matrix<f64>) {}
 
-        // Messy referencing for succint syntax
-        if let (&Some(ref t_data), &Some(ref t_mat), &Some(ref t_out)) = (&self.train_data,
-                                                                          &self.train_mat,
-                                                                          &self.train_output) {
-            let test_mat = self.ker_mat(data, t_data) * t_mat;
-            let post_mean = mean + &test_mat * (t_out - self.mean.func(t_data.clone()));
+    /// Solves an upper triangular linear system.
+    fn solve_u_triangular(mat: &Matrix<f64>, y: &Vector<f64>) -> Vector<f64> {
+        assert!(mat.cols() == y.size(),
+                "Matrix and Vector dimensions do not agree.");
 
-            let post_cov = self.ker_mat(data, data) - test_mat * self.ker_mat(t_data, data);
+        let mut x = vec![0.; y.size()];
 
-            return (post_mean, post_cov);
+        let mut holding_u_sum = 0.;
+        x[y.size() - 1] = y[y.size() - 1] / mat[[y.size() - 1, y.size() - 1]];
+
+        unsafe {
+            for i in (0..y.size() - 1).rev() {
+                holding_u_sum = holding_u_sum + *mat.data().get_unchecked(i * (mat.cols() + 1) + 1);
+                x[i] = (y[i] - holding_u_sum * x[i + 1]) /
+                       *mat.data().get_unchecked(i * (mat.cols() + 1));
+            }
         }
 
-        panic!("The model has not been trained.");
+        Vector::new(x)
+    }
+
+    /// Solves a lower triangular linear system.
+    fn solve_l_triangular(mat: &Matrix<f64>, y: &Vector<f64>) -> Vector<f64> {
+        assert!(mat.cols() == y.size(),
+                "Matrix and Vector dimensions do not agree.");
+
+        let mut x = vec![0.; y.size()];
+
+        let mut holding_l_sum = 0.;
+        x[0] = y[0] / mat[[0, 0]];
+
+        for i in 1..y.size() {
+            holding_l_sum = holding_l_sum + mat.data()[i * (mat.cols() + 1) - 1];
+            x[i] = (y[i] - holding_l_sum * x[i - 1]) / mat.data()[i * (mat.cols() + 1)];
+        }
+
+        Vector::new(x)
     }
 }
