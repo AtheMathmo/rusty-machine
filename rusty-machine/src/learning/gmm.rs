26fa9e88d8e364969bcc98d30c5b6e12456c8b55
James Lucas <LucasJ94@hotmail.co.uk>
2016-03-08 21:26:26 -0800

    Fixing to use options for model parameters

--- rusty-machine/src/learning/gmm.rs
+++ rusty-machine/src/learning/gmm.rs
@@ -14,30 +14,57 @@ pub const SQRT_2_PI: f64 = 2.50662827463100050241576528481104525_f64;
 pub struct GaussianMixtureModel {
     comp_count: usize,
     mix_weights: Vector<f64>,
-    model_means: Matrix<f64>,
-    model_covars: Vec<Matrix<f64>>,
+    model_means: Option<Matrix<f64>>,
+    model_covars: Option<Vec<Matrix<f64>>>,
 }
 
 impl UnSupModel<Matrix<f64>, Matrix<f64>> for GaussianMixtureModel {
     /// Train the model using inputs.
     fn train(&mut self, inputs: &Matrix<f64>) {
-    	// Need to do initialization
+        // Initialization:
+        let k = self.comp_count;
 
-    	for _ in 0..10 {
-    		let weights = self.membership_weights(inputs);
-    		self.update_params(inputs, weights);
-    	}
+        // TODO: Compute sample covariance of the data.
+        // https://en.wikipedia.org/wiki/Sample_mean_and_covariance#Sample_covariance
+        let mut cov_vec = Vec::with_capacity(k);
+        for _ in 0..k {
+            cov_vec.push(Matrix::identity(inputs.cols()));
+        }
+
+        self.model_covars = Some(cov_vec);
+
+        // TODO: Use randomization, or k-means.
+        self.model_means = Some(inputs.select_rows(&(0..k).collect::<Vec<usize>>()[..]));
+
+        for _ in 0..10 {
+            let weights = self.membership_weights(inputs);
+            self.update_params(inputs, weights);
+        }
     }
 
     /// Predict output from inputs.
     fn predict(&self, inputs: &Matrix<f64>) -> Matrix<f64> {
-        self.membership_weights(inputs)
+        if let (&Some(_), &Some(_)) = (&self.model_means, &self.model_covars) {
+            self.membership_weights(inputs)
+        } else {
+            panic!("Model has not been trained.");
+        }
+
     }
 }
 
 impl GaussianMixtureModel {
+    pub fn new(k: usize) -> GaussianMixtureModel {
+        GaussianMixtureModel {
+            comp_count: k,
+            mix_weights: Vector::ones(k) / (k as f64),
+            model_means: None,
+            model_covars: None,
+        }
+    }
+
     fn membership_weights(&self, inputs: &Matrix<f64>) -> Matrix<f64> {
-        let d = self.model_means.cols();
+        let d = inputs.cols();
         let n = inputs.rows();
 
         let pi_d = SQRT_2_PI.powi(d as i32);
@@ -48,39 +75,43 @@ impl GaussianMixtureModel {
         let mut cov_sqrt_dets = Vec::with_capacity(self.comp_count);
         let mut cov_invs = Vec::with_capacity(self.comp_count);
 
-        for k in 0..self.comp_count {
-            // TODO: combine these. We compute det to get the inverse.
-            let covar_det = self.model_covars[k].det();
-            let covar_inv = self.model_covars[k].inverse();
+        if let Some(ref covars) = self.model_covars {
+	        for k in 0..self.comp_count {
+	            // TODO: combine these. We compute det to get the inverse.
+	            let covar_det = covars[k].det();
+	            let covar_inv = covars[k].inverse();
 
-            cov_sqrt_dets.push(covar_det.sqrt());
-            cov_invs.push(covar_inv);
-        }
+	            cov_sqrt_dets.push(covar_det.sqrt());
+	            cov_invs.push(covar_inv);
+	        }
+	    }
 
         // Now we compute the membership weights
-        for i in 0..n {
-            for k in 0..self.comp_count {
+        if let Some(ref means) = self.model_means {
+	        for i in 0..n {
+	            for k in 0..self.comp_count {
 
-                let mut pdf_inv_sum = 0f64;
-                let x_i = inputs.select_rows(&[i]);
+	                let mut pdf_inv_sum = 0f64;
+	                let x_i = inputs.select_rows(&[i]);
 
-                for j in 0..self.comp_count {
-                    if j == k {
-                        continue;
-                    }
+	                for j in 0..self.comp_count {
+	                    if j == k {
+	                        continue;
+	                    }
 
-                    let mu_j = self.model_means.select_rows(&[j]);
-                    let diff = &x_i - mu_j;
+	                    let mu_j = means.select_rows(&[j]);
+	                    let diff = &x_i - mu_j;
 
-                    // diff is 1xd
-                    let exponent = (&diff * &cov_invs[j] * diff.transpose() * 0.5).into_vec()[0];
+	                    // diff is 1 x d
+	                    let exponent = (&diff * &cov_invs[j] * diff.transpose() * 0.5).into_vec()[0];
 
-                    pdf_inv_sum += exponent.exp() * cov_sqrt_dets[j] / self.mix_weights[j];
-                }
+	                    pdf_inv_sum += exponent.exp() * cov_sqrt_dets[j] / self.mix_weights[j];
+	                }
 
-                member_weights_data.push(self.mix_weights[k] * pi_d * pdf_inv_sum);
-            }
-        }
+	                member_weights_data.push(self.mix_weights[k] * pi_d * pdf_inv_sum);
+	            }
+	        }
+	    }
 
         Matrix::new(n, self.comp_count, member_weights_data)
     }
@@ -95,24 +126,28 @@ impl GaussianMixtureModel {
 
         let mut new_means = membership_weights.transpose() * inputs;
 
+        // TODO: Optimize
         for (idx, mean) in new_means.mut_data().chunks_mut(d).enumerate() {
             for i in 0..d {
                 mean[i] = mean[i] / sum_weights[idx];
             }
         }
 
-        self.model_means = new_means;
+        let mut new_covs = Vec::with_capacity(self.comp_count);
 
+        // TODO: Optimize
         for k in 0..self.comp_count {
             let mut cov_mat = Matrix::zeros(d, d);
 
             for i in 0..n {
-                let diff = inputs.select_rows(&[i]) - self.model_means.select_rows(&[k]);
+                let diff = inputs.select_rows(&[i]) - new_means.select_rows(&[k]);
                 cov_mat = cov_mat + (diff.transpose() * diff) * membership_weights[[i, k]];
             }
 
-            self.model_covars[k] = cov_mat / sum_weights[k];
+            new_covs.push(cov_mat / sum_weights[k]);
         }
 
+        self.model_means = Some(new_means);
+        self.model_covars = Some(new_covs);
     }
 }
