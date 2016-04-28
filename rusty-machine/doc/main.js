1f354010070152dc0b198175d0da8b9df028c575
James Lucas <LucasJ94@hotmail.co.uk>
2016-04-27 21:18:57 -0700

    Updating API documentation to v0.2.8

--- rusty-machine/doc/main.js
+++ rusty-machine/doc/main.js
@@ -740,9 +740,7 @@
             $(".search-input").on("keyup input",function() {
                 clearTimeout(searchTimeout);
                 if ($(this).val().length === 0) {
-                    if (browserSupportsHistoryApi()) {
-                        history.replaceState("", "std - Rust", "?search=");
-                    }
+                    window.history.replaceState("", "std - Rust", "?search=");
                     $('#main.content').removeClass('hidden');
                     $('#search.content').addClass('hidden');
                 } else {
@@ -998,7 +996,7 @@
         var prev_id = 0;
 
         function set_fragment(name) {
-            if (browserSupportsHistoryApi()) {
+            if (history.replaceState) {
                 history.replaceState(null, null, '#' + name);
                 $(window).trigger('hashchange');
             } else {
