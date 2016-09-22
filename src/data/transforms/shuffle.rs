c5c06497cd1ed2a5cdb34a753b055c130708456d
James Lucas <LucasJ94@hotmail.co.uk>
2016-09-21 18:54:38 -0700

    Adding tests and docs

--- src/data/transforms/shuffle.rs
+++ src/data/transforms/shuffle.rs
@@ -1,27 +1,118 @@
 //! The Shuffler
-
-/// The Shuffler
-#[derive(Debug)]
-pub struct Shuffler;
+//!
+//! This module contains the unit `Shuffler` struct. This struct implements the
+//! `Transformer` trait and is used to shuffle the rows of an input matrix.
+//! You can also control the random number generator.
+//!
+//! # Examples
+//!
+//! ```
+//! use rusty_machine::linalg::Matrix;
+//! use rusty_machine::data::transforms::Transformer;
+//! use rusty_machine::data::transforms::shuffle::Shuffler;
+//!
+//! // Create an input matrix that we want to shuffle
+//! let mat = Matrix::new(3, 2, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+//!
+//! // Create a new shuffler
+//! let mut shuffler = Shuffler::default();
+//! let shuffled_mat = shuffler.transform(mat).unwrap();
+//!
+//! println!("{}", shuffled_mat);
+//! ```
 
 use learning::LearningResult;
 use linalg::{Matrix, BaseMatrix, BaseMatrixMut};
 use super::Transformer;
 
-use rand::{Rng, thread_rng};
+use rand::{Rng, thread_rng, ThreadRng};
 
-impl<T> Transformer<Matrix<T>> for Shuffler {
-    /// Transforms the inputs and stores the transformation in the Transformer
+/// The `Shuffler`
+///
+/// Provides an implementation of `Transformer` which shuffles
+/// the input rows in place.
+#[derive(Debug)]
+pub struct Shuffler<R: Rng> {
+    rng: R,
+}
+
+impl<R: Rng> Shuffler<R> {
+    /// Construct a new `Shuffler` with given random number generator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// // This doesn't work!
+    /// extern crate rand;
+    ///
+    /// use rusty_machine::data::transforms::Transformer;
+    /// use rusty_machine::data::transforms::shuffle::Shuffler;
+    /// use rand::{IsaacRng, SeedableRng};
+    ///
+    /// // We can create a seeded rng
+    /// let rng = IsaacRng::from_seed(&[1, 2, 3]);
+    ///
+    /// let shuffler = Shuffler::new(rng);
+    /// ```
+    pub fn new(rng: R) -> Self {
+        Shuffler { rng: rng }
+    }
+}
+
+/// Create a new shuffler using the `rand::thread_rng` function
+/// to provide a randomly seeded random number generator.
+impl Default for Shuffler<ThreadRng> {
+    fn default() -> Self {
+        Shuffler { rng: thread_rng() }
+    }
+}
+
+/// The `Shuffler` will transform the input `Matrix` by shuffling
+/// its rows in place.
+///
+/// Under the hood this uses a Fisher-Yates shuffle.
+impl<R: Rng, T> Transformer<Matrix<T>> for Shuffler<R> {
     fn transform(&mut self, mut inputs: Matrix<T>) -> LearningResult<Matrix<T>> {
         let n = inputs.rows();
-        let mut rng = thread_rng();
 
         for i in 0..n {
             // Swap i with a random point after it
-            let j = rng.gen_range(0, n - i);
+            let j = self.rng.gen_range(0, n - i);
             inputs.swap_rows(i, i + j);
         }
 
         Ok(inputs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use linalg::Matrix;
+    use super::super::Transformer;
+    use super::Shuffler;
+
+    use rand::{IsaacRng, SeedableRng};
+
+    #[test]
+    fn seeded_shuffle() {
+        let rng = IsaacRng::from_seed(&[1, 2, 3]);
+        let mut shuffler = Shuffler::new(rng);
+
+        let mat = Matrix::new(4, 2, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
+        let shuffled = shuffler.transform(mat).unwrap();
+
+        assert_eq!(shuffled.into_vec(),
+                   vec![7.0, 8.0, 5.0, 6.0, 3.0, 4.0, 1.0, 2.0]);
+    }
+
+    #[test]
+    fn shuffle_single_row() {
+        let mut shuffler = Shuffler::default();
+
+        let mat = Matrix::new(1, 8, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
+        let shuffled = shuffler.transform(mat).unwrap();
+
+        assert_eq!(shuffled.into_vec(),
+                   vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
     }
 }
