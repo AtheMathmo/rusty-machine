f7f937a9d90ae15382f610b7c5e768a4eba02012
sinhrks <sinhrks@gmail.com>
2016-12-27 19:40:29 +0900

    add trees dataset

--- tests/learning/lin_reg.rs
+++ tests/learning/lin_reg.rs
@@ -49,53 +49,18 @@ fn test_no_train_predict() {
     let _ = lin_mod.predict(&inputs).unwrap();
 }
 
+#[cfg(feature = "datasets")]
 #[test]
-fn test_regression_trees_data() {
-    // test using trees datasets
-    // Atkinson, A. C. (1985) Plots, Transformations and Regression. Oxford University Press.
+fn test_regression_datasets_trees() {
+    use rm::datasets::trees;
+    let trees = trees::load();
 
-    // girth, height
-    let data = matrix![8.3, 70.;
-                       8.6, 65.;
-                       8.8, 63.;
-                       10.5, 72.;
-                       10.7, 81.;
-                       10.8, 83.;
-                       11.0, 66.;
-                       11.0, 75.;
-                       11.1, 80.;
-                       11.2, 75.;
-                       11.3, 79.;
-                       11.4, 76.;
-                       11.4, 76.;
-                       11.7, 69.;
-                       12.0, 75.;
-                       12.9, 74.;
-                       12.9, 85.;
-                       13.3, 86.;
-                       13.7, 71.;
-                       13.8, 64.;
-                       14.0, 78.;
-                       14.2, 80.;
-                       14.5, 74.;
-                       16.0, 72.;
-                       16.3, 77.;
-                       17.3, 81.;
-                       17.5, 82.;
-                       17.9, 80.;
-                       18.0, 80.;
-                       18.0, 80.;
-                       20.6, 87.];
-    let volume = vec![10.3, 10.3, 10.2, 16.4, 18.8, 19.7, 15.6, 18.2, 22.6, 19.9,
-                      24.2, 21.0, 21.4, 21.3, 19.1, 22.2, 33.8, 27.4, 25.7, 24.9,
-                      34.5, 31.7, 36.3, 38.3, 42.6, 55.4, 55.7, 58.3, 51.5, 51.0,
-                      77.0];
     let mut lin_mod = LinRegressor::default();
-    lin_mod.train(&data, &Vector::new(volume)).unwrap();
+    lin_mod.train(&trees.data(), &trees.target()).unwrap();
     let params = lin_mod.parameters().unwrap();
     assert_eq!(params, &Vector::new(vec![-57.98765891838409, 4.708160503017506, 0.3392512342447438]));
 
-    let predicted = lin_mod.predict(&data).unwrap();
+    let predicted = lin_mod.predict(&trees.data()).unwrap();
     let expected = vec![4.837659653793278, 4.55385163347481, 4.816981265588826, 15.874115228921276,
                         19.869008437727473, 21.018326956518717, 16.192688074961563, 19.245949183164257,
                         21.413021404689726, 20.187581283767756, 22.015402271048487, 21.468464618616007,
--- tests/lib.rs
+++ tests/lib.rs
@@ -14,5 +14,4 @@ pub mod learning {
     }
 }
 
-#[cfg(datasets)]
 pub mod datasets;
