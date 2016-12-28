faf59b5a8eb78ae6cb95526c6f1ac4efcd69622b
sinhrks <sinhrks@gmail.com>
2016-12-28 15:56:02 +0900

    ENH: Add classification scores

--- src/analysis/score.rs
+++ src/analysis/score.rs
@@ -5,6 +5,10 @@
 use linalg::{BaseMatrix, Matrix};
 use learning::toolkit::cost_fn::{CostFunc, MeanSqError};
 
+// ************************************
+// Classification Scores
+// ************************************
+
 /// Returns the fraction of outputs which match their target.
 pub fn accuracy<I>(outputs: I, targets: I) -> f64
     where I: ExactSizeIterator,
@@ -24,6 +28,75 @@ pub fn row_accuracy(outputs: &Matrix<f64>, targets: &Matrix<f64>) -> f64 {
     accuracy(outputs.iter_rows(), targets.iter_rows())
 }
 
+/// Returns the precision score for 2 class classification.
+/// true-positive / (true-positive + false-positive)
+pub fn precision<'a, I>(outputs: I, targets: I) -> f64
+    where I: ExactSizeIterator<Item=&'a usize>
+{
+    assert!(outputs.len() == targets.len());
+
+    let mut tpfp = 0.0f64;
+    let mut tp = 0.0f64;
+
+    for (_, &t) in outputs.zip(targets).filter(|&(&o, _)| o == 1) {
+        tpfp += 1.0f64;
+        if t == 1 {
+            tp += 1.0f64;
+        }
+    }
+    tp / tpfp
+}
+
+/// Returns the recall score for 2 class classification.
+/// true-positive / (true-positive + false-negative)
+pub fn recall<'a, I>(outputs: I, targets: I) -> f64
+    where I: ExactSizeIterator<Item=&'a usize>
+{
+    assert!(outputs.len() == targets.len());
+
+    let mut tpfn = 0.0f64;
+    let mut tp = 0.0f64;
+
+    for (&o, _) in outputs.zip(targets).filter(|&(_, &t)| t == 1) {
+        tpfn += 1.0f64;
+        if o == 1 {
+            tp += 1.0f64;
+        }
+    }
+    tp / tpfn
+}
+
+/// Returns the f1 score for 2 class classification
+/// 2 * precision * recall / (precision + recall)
+pub fn f1<'a, I>(outputs: I, targets: I) -> f64
+    where I: ExactSizeIterator<Item=&'a usize>
+{
+    assert!(outputs.len() == targets.len());
+
+    let mut tpos = 0.0f64;
+    let mut fpos = 0.0f64;
+    let mut fneg = 0.0f64;
+
+    for (&o, &t) in outputs.zip(targets) {
+        if (o == 1) & (t == 1) {
+            tpos += 1.0f64;
+        } else if t == 1 {
+            fpos += 1.0f64;
+        } else if o == 1 {
+            fneg += 1.0f64;
+        }
+    }
+    // precision
+    let p = tpos / (tpos + fpos);
+    // recall
+    let r = tpos / (tpos + fneg);
+    2.0 * p * r / (p + r)
+}
+
+// ************************************
+// Regression Scores
+// ************************************
+
 // TODO: generalise to accept arbitrary iterators of diff-able things
 /// Returns the additive inverse of the mean-squared-error of the
 /// outputs. So higher is better, and the returned value is always
@@ -37,13 +110,74 @@ pub fn neg_mean_squared_error(outputs: &Matrix<f64>, targets: &Matrix<f64>) -> f
 #[cfg(test)]
 mod tests {
     use linalg::Matrix;
-    use super::{accuracy, neg_mean_squared_error};
+    use super::{accuracy, precision, recall, f1, neg_mean_squared_error};
 
     #[test]
     fn test_accuracy() {
         let outputs = [1, 2, 3, 4, 5, 6];
         let targets = [1, 2, 3, 3, 5, 1];
         assert_eq!(accuracy(outputs.iter(), targets.iter()), 2f64/3f64);
+
+        let outputs = [1, 1, 1, 0, 0, 0];
+        let targets = [1, 1, 1, 0, 0, 1];
+        assert_eq!(accuracy(outputs.iter(), targets.iter()), 5.0f64 / 6.0f64);
+    }
+
+    #[test]
+    fn test_precision() {
+        let outputs = [1, 1, 1, 0, 0, 0];
+        let targets = [1, 1, 0, 0, 1, 1];
+        assert_eq!(precision(outputs.iter(), targets.iter()), 2.0f64 / 3.0f64);
+
+        let outputs = [1, 1, 1, 0, 1, 1];
+        let targets = [1, 1, 0, 0, 1, 1];
+        assert_eq!(precision(outputs.iter(), targets.iter()), 0.8);
+
+        let outputs = [0, 0, 0, 1, 1, 1];
+        let targets = [1, 1, 1, 1, 1, 0];
+        assert_eq!(precision(outputs.iter(), targets.iter()), 2.0f64 / 3.0f64);
+
+        let outputs = [1, 1, 1, 1, 1, 0];
+        let targets = [0, 0, 0, 1, 1, 1];
+        assert_eq!(precision(outputs.iter(), targets.iter()), 0.4);
+    }
+
+    #[test]
+    fn test_recall() {
+        let outputs = [1, 1, 1, 0, 0, 0];
+        let targets = [1, 1, 0, 0, 1, 1];
+        assert_eq!(recall(outputs.iter(), targets.iter()), 0.5);
+
+        let outputs = [1, 1, 1, 0, 1, 1];
+        let targets = [1, 1, 0, 0, 1, 1];
+        assert_eq!(recall(outputs.iter(), targets.iter()), 1.0);
+
+        let outputs = [0, 0, 0, 1, 1, 1];
+        let targets = [1, 1, 1, 1, 1, 0];
+        assert_eq!(recall(outputs.iter(), targets.iter()), 0.4);
+
+        let outputs = [1, 1, 1, 1, 1, 0];
+        let targets = [0, 0, 0, 1, 1, 1];
+        assert_eq!(recall(outputs.iter(), targets.iter()), 2.0f64 / 3.0f64);
+    }
+
+    #[test]
+    fn test_f1() {
+        let outputs = [1, 1, 1, 0, 0, 0];
+        let targets = [1, 1, 0, 0, 1, 1];
+        assert_eq!(f1(outputs.iter(), targets.iter()), 0.57142857142857151);
+
+        let outputs = [1, 1, 1, 0, 1, 1];
+        let targets = [1, 1, 0, 0, 1, 1];
+        assert_eq!(f1(outputs.iter(), targets.iter()), 0.88888888888888895);
+
+        let outputs = [0, 0, 0, 1, 1, 1];
+        let targets = [1, 1, 1, 1, 1, 0];
+        assert_eq!(f1(outputs.iter(), targets.iter()), 0.5);
+
+        let outputs = [1, 1, 1, 1, 1, 0];
+        let targets = [0, 0, 0, 1, 1, 1];
+        assert_eq!(f1(outputs.iter(), targets.iter()), 0.5);
     }
 
     #[test]
